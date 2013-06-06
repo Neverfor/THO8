7edba3da9e7ff1750a80b003929355652d6c4d8e
Laurens van der Kevie <ljvanderkevie@gmail.com>
2013-06-06 17:04:20 +0200

    TaxiService

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using TestingConsole.TaxiService;
 
 namespace TestingConsole
 {
@@ -11,32 +12,12 @@ namespace TestingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("start");
-            try
-            {
-                SqlConnection thisConnection = new SqlConnection(@"Network Library=DBMSSOCN;Data Source=apv8jive40.database.windows.net,1433;database=team3-to8;User id=michael@apv8jive40;Password=Ditisonzedatabase!;");
-                thisConnection.Open();
-                //start by creating new command
-                SqlCommand thisCommand = thisConnection.CreateCommand();
-                Console.WriteLine(thisCommand.CommandText);
-                //insert new row to table "TaxiBooking"
-                //for testing will be used random id generator, taxiID will always be 999.
-                Random random = new Random();
-                int rand = random.Next(1, 999999);
-                int Id = rand;
-                int TaxiId = 2;
-
-                //link it to user [Not yet implemented] 
-                thisCommand.CommandText = "INSERT INTO TaxiBooking values('" + Id + "', '" + TaxiId + "')";
-                Console.WriteLine(thisCommand.ExecuteNonQuery());
-                thisConnection.Close();
-                Console.WriteLine("success");
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadLine();
+            TaxiServiceClient client = new TaxiServiceClient();
+            ServicesDataContracts.TaxiBookingRequest tbr = new ServicesDataContracts.TaxiBookingRequest();
+            tbr.DateTime = new DateTime();
+            tbr.IsDepartureTime = true;
+            tbr.AmountOfPassengers = 2;
+            client.DoTaxiBooking(tbr);
         }
     }
 }
