c025c0e71203d45ab2137de76dc957c5a50f52e3
Laurens van der Kevie <ljvanderkevie@gmail.com>
2013-06-06 14:01:55 +0200

    database connection test... more to come

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace TestingConsole
 {
@@ -10,7 +11,31 @@ namespace TestingConsole
     {
         static void Main(string[] args)
         {
-            // for all the testing!
+            Console.WriteLine("start");
+            try
+            {
+                SqlConnection thisConnection = new SqlConnection(@"Network Library=DBMSSOCN;Data Source=apv8jive40.database.windows.net,1433;database=team3-to8;User id=michael@apv8jive40;Password=Ditisonzedatabase!;");
+                thisConnection.Open();
+                //start by creating new command
+                SqlCommand thisCommand = thisConnection.CreateCommand();
+                Console.WriteLine(thisCommand.CommandText);
+                //insert new row to table "TaxiBooking"
+                //for testing will be used random id generator, taxiID will always be 999.
+                Random random = new Random();
+                int Id = 1;
+                int TaxiId = 2;
+
+                //link it to user [Not yet implemented] 
+                thisCommand.CommandText = "INSERT INTO TaxiBooking values('" + Id + "', '" + TaxiId + "')";
+                Console.WriteLine(thisCommand.ExecuteNonQuery());
+                thisConnection.Close();
+                Console.WriteLine("success");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
