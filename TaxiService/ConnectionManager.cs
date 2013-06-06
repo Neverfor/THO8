b51565235359ce78cd7ddf8e69a8df3893312278
Laurens van der Kevie <ljvanderkevie@gmail.com>
2013-06-06 14:18:54 +0200

    connectionManager added

---
+++ TaxiService/ConnectionManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace TaxiService
+{
+    public class ConnectionManager
+    {
+        private static ConnectionManager instance;
+        private SqlConnection con;
+
+        private ConnectionManager()
+        {
+            con = new SqlConnection(@"Network Library=DBMSSOCN;" +
+                                    "Data Source=apv8jive40.database.windows.net,1433;" +
+                                    "database=team3-to8;" +
+                                    "User id=michael@apv8jive40;" +
+                                    "Password=Ditisonzedatabase!;"
+                                    );
+        }
+
+        public static ConnectionManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ConnectionManager();
+                }
+                return instance;
+            }
+        }
+
+        public 
+    }
+}
