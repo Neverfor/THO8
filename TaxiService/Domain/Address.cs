86c860cc4f5aa0e1ddeafe1ef4882a3f507850f4
Laurens van der Kevie <ljvanderkevie@gmail.com>
2013-06-07 16:32:36 +0200

    perfectly transported

--- TaxiService/Domain/Address.cs
+++ TaxiService/Domain/Address.cs
@@ -11,12 +11,16 @@ namespace TaxiService.Domain
     {
         [DataMember]
         public string Street { get; set; }
+
         [DataMember]
         public int Number { get; set; }
+
         [DataMember(IsRequired = true)]
         public string City { get; set; }
+
         [DataMember(IsRequired = true)]
         public string Country { get; set; }
+
         [DataMember]
         public string ZipCode { get; set; }
 
