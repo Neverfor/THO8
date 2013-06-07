210afd5ca996aeb7a9df1d826cf4594e893f791e
Michael de Vreeze <michaeldevreeze@gmail.com>
2013-06-07 15:39:46 +0200

    new project

---
+++ TaxiService/Domain/TaxiBooking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using TaxiService.ServicesDataContracts;
+
+namespace TaxiService.Domain
+{
+    [DataContract]
+    public class TaxiBooking : TaxiPriceInfo
+    {
+
+    }
+}
