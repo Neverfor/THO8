b9412cbab5d6cd02d6c4e947af935e88b75c8f68
Laurens van der Kevie <ljvanderkevie@gmail.com>
2013-06-07 14:29:27 +0200

    fixed DistanceManager

--- TaxiService/DistanceManager.cs
+++ TaxiService/DistanceManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,7 +25,6 @@ namespace TaxiService
                 return instance;
             }
         }
-
         public double DistanceInKmBetween(string departurePoint, string destination)
         {
             using (WebClient wc = new WebClient())
