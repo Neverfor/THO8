1b10a305208685d8828d87cb8656d2c3da7b5f76
Michael de Vreeze <michaeldevreeze@gmail.com>
2013-06-11 12:14:36 +0200

    Cleanup

---
+++ TaxiService/Domain/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace TaxiService.Domain
+{
+    [DataContract]
+    public class Address
+    {
+        [DataMember]
+        public string Street { get; set; }
+
+        [DataMember]
+        public int Number { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string City { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Country { get; set; }
+
+        [DataMember]
+        public string ZipCode { get; set; }
+
+        public Address()
+        {
+
+        }
+    }
+
+}
