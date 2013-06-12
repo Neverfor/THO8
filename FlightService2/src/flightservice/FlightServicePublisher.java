5443e81dee7c2a89818ade55c8ea38151199a158
SpenQ <frankplattel@gmail.com>
2013-06-12 16:11:17 +0200

    Webservice added
    Endpoint not yet implemented

--- FlightService2/src/flightservice/FlightServicePublisher.java
+++ FlightService2/src/flightservice/FlightServicePublisher.java
@@ -9,11 +9,6 @@ import flightservice.FlightServiceGetAirportsExceptionFaultFaultMessage;
 public class FlightServicePublisher {
 	
 	public static void main(String[] args) throws FlightServiceGetAirportsExceptionFaultFaultMessage {
-		 
-		FlightService_Service flightService = new FlightService_Service();
-		FlightService flight = flightService.getBasicHttpBindingFlightService();
- 
-		System.out.println(flight.toString());
 		
 		Endpoint.publish("http://localhost:9999/ws/flight", new FlightService_Service());
     }
