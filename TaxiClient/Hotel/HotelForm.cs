7de33dd3bb4869e20756ecad9bd3d09f778a929f
Neverfor <icelance@hotmail.ru>
2013-06-18 14:33:28 +0200

    getHotels fix

--- TaxiClient/Hotel/HotelForm.cs
+++ TaxiClient/Hotel/HotelForm.cs
@@ -31,13 +31,12 @@ namespace TaxiClient
         private void button1_Click(object sender, EventArgs e)
         {
             
-            //Hotel hotel = new Hotel();
            Hotel[] hotels;
              
             using (HotelBookingServiceClient client = new HotelBookingServiceClient())
             {
-                String name = "";
-                String location = "";
+                String name = textBox1.ToString();
+                String location = textBox2.ToString();
                 
                 hotels = client.getHotels(name, location);
                // hotels = client.getHotelsAsync(name, location);
@@ -45,7 +44,7 @@ namespace TaxiClient
                 foreach(Hotel hotel in hotels) 
                 {
                     hotelList += "\n" + hotel.Location.ToString() +
-                    hotel.Name.ToString();
+                    " " + hotel.Name.ToString();
                 }
                 MessageBox.Show("The following hotels were found: " + hotelList);
 
