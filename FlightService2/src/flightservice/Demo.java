dfa9bad4a2eb8bd5e6f72c8f60a04910b95184d0
SpenQ <frankplattel@gmail.com>
2013-06-20 13:29:57 +0200

    foutje

--- FlightService2/src/flightservice/Demo.java
+++ FlightService2/src/flightservice/Demo.java
@@ -12,21 +12,29 @@ import javax.xml.bind.Unmarshaller;
 public class Demo {
  
     public static void main(String[] args) throws Exception {
-        JAXBContext jc = JAXBContext.newInstance(AirportDTO.class);
+        JAXBContext jc = JAXBContext.newInstance(ArrayOfAirportDTO.class);
  
-        Unmarshaller unmarshaller2 = jc.createUnmarshaller();
-        ArrayOfAirportDTO airportArray = (ArrayOfAirportDTO) unmarshaller2.unmarshal(new File("airports.xml"));
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        ArrayOfAirportDTO airportArray = (ArrayOfAirportDTO) unmarshaller.unmarshal(new File("airports.xml"));
+        
         System.out.println(airportArray.getAirportDTO());
         
-        Unmarshaller unmarshaller = jc.createUnmarshaller();
-        AirportDTO airportdto = (AirportDTO) unmarshaller.unmarshal(new File("airports.xml"));
+        Marshaller marshaller = jc.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(airportArray, System.out);
+        
+        // ---
+        JAXBContext jc2 = JAXBContext.newInstance(AirportDTO.class);
+        
+        Unmarshaller unmarshaller2 = jc2.createUnmarshaller();
+        AirportDTO airportdto = (AirportDTO) unmarshaller2.unmarshal(new File("airports.xml"));
  
         System.out.println("code:  " + airportdto.getAirportId());
         System.out.println("airport:   " + airportdto.getName());
 //        System.out.println("country:   " + item.getCountry());
  
-        Marshaller marshaller = jc.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.marshal(airportdto, System.out);
+        Marshaller marshaller2 = jc2.createMarshaller();
+        marshaller2.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller2.marshal(airportdto, System.out);
     }
 }
